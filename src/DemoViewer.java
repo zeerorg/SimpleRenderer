622ab95d7c817ca9749ce9a3c40233359d31df29
Rishabh Gupta <rishabh4325@gmil.com>
2017-04-03 17:40:42 -0700

    Added rendering with z-buffer

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -23,72 +23,82 @@ public class DemoViewer {
         pane.add(pitchSlider, BorderLayout.EAST);
 
         JPanel renderPanel = new JPanel() {
-          public void paintComponent(Graphics g){
+            public void paintComponent(Graphics g) {
 
-              BufferedImage img =
-                      new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+                BufferedImage img =
+                        new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
 
-              double heading = Math.toRadians(headingSlider.getValue());
-              double heading2 = Math.toRadians(pitchSlider.getValue());
-              Matrix3 transform_temp = new Matrix3(new double[] {
-                      Math.cos(heading), 0, -Math.sin(heading),
-                      0, 1, 0,
-                      Math.sin(heading), 0, Math.cos(heading)
-              });
-              Matrix3 transform2 = new Matrix3(new double[] {
-                      1, 0, 0,
-                      0, Math.cos(heading2), Math.sin(heading2),
-                      0, -Math.sin(heading2), Math.cos(heading2)
-              });
-              Matrix3 transform = transform_temp.multiply(transform2);
+                double heading = Math.toRadians(headingSlider.getValue());
+                double heading2 = Math.toRadians(pitchSlider.getValue());
+                Matrix3 transform_temp = new Matrix3(new double[]{
+                        Math.cos(heading), 0, -Math.sin(heading),
+                        0, 1, 0,
+                        Math.sin(heading), 0, Math.cos(heading)
+                });
+                Matrix3 transform2 = new Matrix3(new double[]{
+                        1, 0, 0,
+                        0, Math.cos(heading2), Math.sin(heading2),
+                        0, -Math.sin(heading2), Math.cos(heading2)
+                });
+                Matrix3 transform = transform_temp.multiply(transform2);
 
-              Graphics2D g2D = (Graphics2D) g;
-              g2D.setColor(Color.BLACK);
-              g2D.fillRect(0, 0, getWidth(), getHeight());
-              g2D.setColor(Color.WHITE);
-              for(Triangle t: getTetrahedron()) {
-                  Vertex v1 = transform.transform(t.v1);
-                  Vertex v2 = transform.transform(t.v2);
-                  Vertex v3 = transform.transform(t.v3);
+                Graphics2D g2D = (Graphics2D) g;
+                g2D.setColor(Color.BLACK);
+                g2D.fillRect(0, 0, getWidth(), getHeight());
+                g2D.setColor(Color.WHITE);
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+                for (int q = 0; q < zBuffer.length; q++) {
+                    zBuffer[q] = Double.NEGATIVE_INFINITY;
+                }
+                for (Triangle t : getTetrahedron()) {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
 
-                  // since we are not using Graphics2D anymore,
-                  // we have to do translation manually
-                  v1.x += getWidth() / 2;
-                  v1.y += getHeight() / 2;
-                  v2.x += getWidth() / 2;
-                  v2.y += getHeight() / 2;
-                  v3.x += getWidth() / 2;
-                  v3.y += getHeight() / 2;
+                    // since we are not using Graphics2D anymore,
+                    // we have to do translation manually
+                    v1.x += getWidth() / 2;
+                    v1.y += getHeight() / 2;
+                    v2.x += getWidth() / 2;
+                    v2.y += getHeight() / 2;
+                    v3.x += getWidth() / 2;
+                    v3.y += getHeight() / 2;
 
-                  int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
-                  int maxX = (int) Math.min(img.getWidth() - 1,
-                          Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
-                  int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
-                  int maxY = (int) Math.min(img.getHeight() - 1,
-                          Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+                    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                    int maxX = (int) Math.min(img.getWidth() - 1,
+                            Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+                    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getHeight() - 1,
+                            Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
 
-                  double triangleArea =
-                          (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+                    double triangleArea =
+                            (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+                    for (int y = minY; y <= maxY; y++) {
+                        for (int x = minX; x <= maxX; x++) {
+                            double b1 =
+                                    ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                            double b2 =
+                                    ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                            double b3 =
+                                    ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                                // handle rasterization...
+                                // for each rasterized pixel:
+                                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                                int zIndex = y * img.getWidth() + x;
+                                if (zBuffer[zIndex] < depth) {
+                                    img.setRGB(x, y, t.color.getRGB());
+                                    zBuffer[zIndex] = depth;
+                                }
+                            }
+                        }
+                    }
 
 
-                  for (int y = minY; y <= maxY; y++) {
-                      for (int x = minX; x <= maxX; x++) {
-                          double b1 =
-                                  ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
-                          double b2 =
-                                  ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
-                          double b3 =
-                                  ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
-                          if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
-                              img.setRGB(x, y, t.color.getRGB());
-                          }
-                      }
-                  }
-
-                g2D.drawImage(img, 0, 0, null);
-              }
-              // Main Rendering here
-          }
+                    g2D.drawImage(img, 0, 0, null);
+                }
+            }
         };
 
         headingSlider.addChangeListener(e -> renderPanel.repaint());
