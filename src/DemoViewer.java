c2a88a32305c34c10616c5fee62c01defcc05ac9
Rishabh Gupta <rishabh4325@gmil.com>
2017-04-03 17:32:53 -0700

    Rasterization using baycentric algorithm without z-buffer

--- src/DemoViewer.java
+++ src/DemoViewer.java
@@ -45,19 +45,47 @@ public class DemoViewer {
               Graphics2D g2D = (Graphics2D) g;
               g2D.setColor(Color.BLACK);
               g2D.fillRect(0, 0, getWidth(), getHeight());
-              g2D.translate(getWidth()/2, getHeight()/2);
               g2D.setColor(Color.WHITE);
               for(Triangle t: getTetrahedron()) {
                   Vertex v1 = transform.transform(t.v1);
                   Vertex v2 = transform.transform(t.v2);
                   Vertex v3 = transform.transform(t.v3);
 
-                  Path2D path = new Path2D.Double();
-                  path.moveTo(v1.x, v1.y);
-                  path.lineTo(v2.x, v2.y);
-                  path.lineTo(v3.x, v3.y);
-                  path.closePath();
-                  g2D.draw(path);
+                  // since we are not using Graphics2D anymore,
+                  // we have to do translation manually
+                  v1.x += getWidth() / 2;
+                  v1.y += getHeight() / 2;
+                  v2.x += getWidth() / 2;
+                  v2.y += getHeight() / 2;
+                  v3.x += getWidth() / 2;
+                  v3.y += getHeight() / 2;
+
+                  int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                  int maxX = (int) Math.min(img.getWidth() - 1,
+                          Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+                  int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                  int maxY = (int) Math.min(img.getHeight() - 1,
+                          Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+
+                  double triangleArea =
+                          (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+
+                  for (int y = minY; y <= maxY; y++) {
+                      for (int x = minX; x <= maxX; x++) {
+                          double b1 =
+                                  ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                          double b2 =
+                                  ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                          double b3 =
+                                  ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+                          if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                              img.setRGB(x, y, t.color.getRGB());
+                          }
+                      }
+                  }
+
+                g2D.drawImage(img, 0, 0, null);
               }
               // Main Rendering here
           }
